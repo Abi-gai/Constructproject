764275e2abe12f0b51c5586f900080065bbcf6d5
Abi-gai <93228698+Abi-gai@users.noreply.github.com>
2022-12-07 18:45:38 +0000

    Add files via upload

---
+++ config/dbconn.php
@@ -0,0 +1,11 @@
+<?php
+$dbconn = mysqli_connect("localhost","id19879284_root","u]l!-t5yA4)f~n^4","id19879284_electricks");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+  date_default_timezone_set("Asia/Manila"); 
+?>
